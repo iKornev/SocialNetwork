9f2bcc0df76de67241ca2c289763ed3687bd9983
iKornev <ik0077ik25@gmail.com>
2021-03-12 17:01:48 +0200

    added comonents: Header, Navbar, Profile

---
+++ social-network/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+          <img src='https://pngicon.ru/file/uploads/vinni-pukh-v-png-256x256.png' />
+        </header >
+    );
+
+}
+
+export default Header;
---
+++ social-network/src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile  = () => {
+    return (
+      <div className='content'>
+          <div>
+          <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg" width="100%" height="300"/>
+          </div>
+          <div>
+            ava + description 
+          </div>
+          <div>
+            My posts
+            <div>
+              New post 
+            </div> 
+            <div>
+              post1
+            </div>
+            <div>
+              post2
+            </div>
+          </div>
+        </div>
+      
+    );
+}
+
+export default Profile ;
